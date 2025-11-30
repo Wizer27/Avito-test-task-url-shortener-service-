06a9e9360a1d721d5ff9943a1f6e7420fa6f7166
Wizer27 <svd2yar@gmail.com>
2025-11-30 19:23:22 +0300

    init api

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from fastapi import FastAPI,Header,HTTPException,status
 from fastapi.responses import RedirectResponse
+import uvicorn
+import string
+from secrets import choice
+
+def random_slug() -> str:
+    ALP = string.ascii_letters + string.digits
+    res = ""
+    for _ in range(6):
+        res += choice(ALP)
+    return res    
+
+print(random_slug())
+
 
 
 app = FastAPI()
@@ -7,3 +20,8 @@ app = FastAPI()
 @app.get("/")
 async def main():
     return {"API":"URL SHORTENER"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app,host = "0.0.0.0",port = 8080)
+
